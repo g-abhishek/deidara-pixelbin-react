e02fa2e7ff36123e00a8c762715cf42422a9885c
Aditya Baranwal <baranwal.adi@gmail.com>
2022-01-19 13:13:37 +0530

    [Auto Generated] 1.0.0

---
+++ repos/javascript/plugins/Artifact.js
@@ -0,0 +1,9 @@
+import Transformation from "../transformation.js";
+
+export const remove = function ({}) {
+    return new Transformation([`af.remove()`]);
+};
+
+export default {
+    remove,
+};
---
+++ repos/javascript/plugins/WatermarkRemoval.js
@@ -0,0 +1,9 @@
+import Transformation from "../transformation.js";
+
+export const remove = function ({}) {
+    return new Transformation([`wm.remove()`]);
+};
+
+export default {
+    remove,
+};
