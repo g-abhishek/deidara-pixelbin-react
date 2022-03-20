5a59d889aa4475484105b15ea4fab2d25fbcbf73
Noel Mathew <noelmathew@gofynd.com>
2022-03-20 20:29:32 +0530

    updated default host to prod

--- output/javascript/utils/transformation.utils.js
+++ output/javascript/utils/transformation.utils.js
@@ -9,6 +9,7 @@ export const getImageUrlWithOptions = function (url, pattern, version = "v2") {
         );
     urlParts["pattern"] = pattern || "original";
     urlParts["version"] = version || "v2";
+    urlParts["host"] = "https://cdn.pixelbin.io";
     const urlKeySorted = ["host", "version", "cloudName", "zoneSlug", "pattern", "filePath"];
     const urlArr = [];
     urlKeySorted.forEach((key) => {
