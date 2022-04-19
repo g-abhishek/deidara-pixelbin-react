be94733cbd8ffbbcc407f61c60187b416de6e50f
utkarshgupta <utkarshgupta@gofynd.com>
2022-04-19 09:16:05 +0530

    bug fixes, 2 test cases failing

--- artifacts/react/dist/index.js
+++ artifacts/react/dist/index.js
@@ -3,21 +3,16 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "PixelBinDownloadButton", {
-  enumerable: true,
-  get: function () {
-    return _PixelBinDownloadButton.default;
-  }
+
+var _components = require("./components");
+
+Object.keys(_components).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _components[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return _components[key];
+    }
+  });
 });
-Object.defineProperty(exports, "PixelBinImage", {
-  enumerable: true,
-  get: function () {
-    return _PixelBinImage.default;
-  }
-});
-
-var _PixelBinImage = _interopRequireDefault(require("./PixelBinImage"));
-
-var _PixelBinDownloadButton = _interopRequireDefault(require("./PixelBinDownloadButton"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
