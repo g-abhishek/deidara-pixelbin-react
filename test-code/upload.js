fcffcb021125fa12e754df2a4bee61a3c36ed158
abhishekgupta01 <abhishekgupta@gofynd.com>
2022-05-19 20:38:38 +0530

    moved outside

--- test-code/upload.js
+++
@@ -1,4 +0,0 @@
-import upload from "../artifacts/javascript/upload";
-
-const fileInput = document.getElementById("fileUpload");
-fileInput.addEventListener("change", (e) => upload(e.target.files[0]));
